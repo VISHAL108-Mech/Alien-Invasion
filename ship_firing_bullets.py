29f6437c9806e5d0d1a581f0fd56617c2778e40f
VISHAL108-Mech <vy4122000@gmail.com>
2025-09-18 16:34:51 +0530

    modified

--- ship_firing_bullets.py
+++ ship_firing_bullets.py
@@ -6,14 +6,13 @@ class AlienInvasion:
 
     def __init__(self):
         """Initialize/set up the game, and create game assets."""
-        pygame.init() # 1 Initializes pygame's background settings.
-        print("Pygame initialized.")
+        pygame.init() # Initializes pygame's background settings.
+        self.clock = pygame.time.Clock() # Creating a clock object to set FPS rate.
+        self.screen = pygame.display.set_mode((1535, 784)) # Sets the background window in pixels.
+        pygame.display.set_caption("Alien Invasion") # It shows the title of the game above the bar of the window.
+        self.bg_color = (230, 230, 230) # Sets the background color.
 
-        self.screen = pygame.display.set_mode((1535, 785))
-        pygame.display.set_caption("Alien Invasion")
-
-    @staticmethod
-    def run_game():
+    def run_game(self):
         """Start the main loop for the game."""
         running = True
         while running:
@@ -21,11 +20,15 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            # Redraw the screen and fill it with background color during each pass through the loop.
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            pygame.display.flip() # Updates the screen and show's it to the player.
+            self.clock.tick(60) # Tells the game to run at 60 FPS.
         pygame.quit()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # It runs the file if its main not imported.
      # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
